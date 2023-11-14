d5538b95256a2d2d8e566617061041ea3770e829
unknown <Alexia@DESKTOP-AP18MAI.info.local>
2023-11-14 18:36:08 -0300

    initial commit

---
+++ bolosjulia-react/src/pages/ListaBolo/index.tsx
@@ -0,0 +1,26 @@
+//estilização
+import "./style.css";
+
+//componentes
+
+//api
+
+//hooks
+
+
+
+function ListaBolo() {
+
+    return (
+        <main>
+            <section className="lista container">
+                <h1>Lista de Bolos</h1>
+
+                {/* CARDBOLO */}
+
+            </section>
+        </main>
+    );
+};
+
+export default ListaBolo;
